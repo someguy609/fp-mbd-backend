b1b30c5a105aa47966920d6a4b659f9322fee46d
Amtsal99 <amtsal.99@gmail.com>
2025-06-16 20:50:36 +0700

    fix: fix user routes and endpoints

--- routes/user_route.go
+++ routes/user_route.go
@@ -14,17 +14,20 @@ func User(route *gin.Engine, injector *do.Injector) {
 	jwtService := do.MustInvokeNamed[service.JWTService](injector, constants.JWTService)
 	userController := do.MustInvoke[controller.UserController](injector)
 
+	authRoutes := route.Group("/api")
+	{
+		// Auth
+		authRoutes.POST("/register", userController.Register)
+		authRoutes.POST("/login", userController.Login)
+		authRoutes.GET("/me", middleware.Authenticate(jwtService), userController.Me)
+		// authRoutes.POST("/logout", middleware.Authenticate(jwtService), userController.Logout)
+	}
 	routes := route.Group("/api/user")
 	{
 		// User
-		routes.POST("", userController.Register)
-		routes.GET("", userController.GetAllUser)
-		routes.POST("/login", userController.Login)
-		// routes.POST("/refresh", userController.Refresh)
-		routes.DELETE("", middleware.Authenticate(jwtService), userController.Delete)
-		routes.PATCH("", middleware.Authenticate(jwtService), userController.Update)
-		routes.GET("/me", middleware.Authenticate(jwtService), userController.Me)
-		// routes.POST("/verify_email", userController.VerifyEmail)
-		// routes.POST("/send_verification_email", userController.SendVerificationEmail)
+		routes.GET("", middleware.Authenticate(jwtService), userController.GetAllUser) // admin only
+		routes.GET("/:user_id", userController.GetUserByUserId)                        // admin only
+		routes.PATCH("/:user_id", middleware.Authenticate(jwtService), userController.Update)
+		routes.DELETE("/:user_id", middleware.Authenticate(jwtService), userController.Delete) // admin only
 	}
 }
